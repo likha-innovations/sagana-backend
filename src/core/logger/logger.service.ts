20f152295981054087e317ce6305f6dedc22ba8f
zchuwie <mayaloveptr.0108@gmail.com>
2026-08-10 22:56:35 +0800

    chore(core): format and refine global exception filter and logger

--- src/core/logger/logger.service.ts
+++ src/core/logger/logger.service.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Injectable, ConsoleLogger } from '@nestjs/common';
 import { promises as fs } from 'fs';
 import * as path from 'path';
