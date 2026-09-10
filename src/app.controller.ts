60359b672777354946f9b998388365dc2defb062
zchuwie <mayaloveptr.0108@gmail.com>
2026-09-10 15:43:17 +0800

    feat: add root health route and packageManager

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,8 +4,14 @@ import { Public } from './core/decorators/public.decorator';
 @Controller()
 export class AppController {
   @Public()
+  @Get()
+  getRoot() {
+    return { status: 'ok', message: 'Sagana Backend API is running', uptime: process.uptime() };
+  }
+
+  @Public()
   @Get('health')
-  getHello(): any {
+  getHealth() {
     return { status: 'ok', uptime: process.uptime() };
   }
 }
